42f41f782f52b7920b5dff0b21851cc28c415580
Andrey <muriy.a@outlook.com>
2020-01-11 23:23:05 +0300

    Modify create threads tests

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -12,21 +12,15 @@ class CreateThreadsTest extends TestCase
     /** @test */
     public function guest_may_not_create_threads()
     {
-        $this->expectException('\Illuminate\Auth\AuthenticationException');
+        $this->withExceptionHandling();
 
-        /** @var \App\Thread $thread */
-        $thread = make('App\Thread');
-        $this->post('/threads', $thread->toArray());
-    }
+        $this->post('/threads')
+            ->assertRedirect('/login');
 
-    /** @test */
-    public function guest_can_not_see_the_thread_create_page()
-    {
-        $this->withExceptionHandling()
-            ->get('/threads/create')
+        $this->get('/threads/create')
             ->assertRedirect('/login');
     }
-    
+
     /** @test */
     public function an_authenticated_user_can_create_new_forum_threads()
     {
