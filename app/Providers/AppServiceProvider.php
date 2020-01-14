80c44cf0eed2a5719ce6f3b3e30edb8d7f67c03f
Andrey <muriy.a@outlook.com>
2020-01-14 23:17:19 +0300

    Exclude unnecessary loadings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,14 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
+     * Массив представлений, для которых не нужен массив channel
+     * @var array
+     */
+    protected $excludes = [
+        'threads.reply'
+    ];
+
+    /**
      * Register any application services.
      *
      * @return void
@@ -25,7 +33,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \View::composer('*', function ($view) {
-            $view->with('channels', Channel::all());
+            if (!in_array($view->getName(), $this->excludes)) {
+                $view->with('channels', Channel::all());
+            }
         });
     }
 }
