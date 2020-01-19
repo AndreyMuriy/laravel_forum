7d64cf9f1d37f66850e6ade290e57f428a0fc8df
Andrey <muriy.a@outlook.com>
2020-01-19 21:24:31 +0300

    Add thread policy

--- resources/views/threads/thread.blade.php
+++ resources/views/threads/thread.blade.php
@@ -8,17 +8,15 @@
             <span>{{ $thread->created_at->diffForHumans() }}</span>
             </span>
 
-            @auth()
-                @if (auth()->id() == $thread->creator->id)
-                    <div>
-                        <form method="POST" action="{{ $thread->path() }}">
-                            {{ csrf_field() }}
-                            {{ method_field('DELETE') }}
-                            <button type="submit" class="btn btn-link">Delete</button>
-                        </form>
-                    </div>
-                @endif
-            @endauth
+            @can('update', $thread)
+                <div>
+                    <form method="POST" action="{{ $thread->path() }}">
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE') }}
+                        <button type="submit" class="btn btn-link">Delete</button>
+                    </form>
+                </div>
+            @endcan
 
         </div>
         <h4>{{ $thread->title }}</h4>
